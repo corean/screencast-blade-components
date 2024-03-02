5c0410da363e091cd863d4cfd5073a33daac588b
corean <corean@corean.biz>
2024-03-02 18:26:26 +0900

    ep.3 Slots

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,4 @@
-<!doctype html>
-<html lang="{{ config('app.locale')  }}">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    @vite('resources/css/app.css')
-</head>
-<body>
-<div class="max-w-2xl mx-auto py-16">
+<x-layout.app>
     <div class="grid gap-3">
         <h2 class="text-5xl font-extrabold ">Separator</h2>
 
@@ -23,6 +12,4 @@
             Team
         </div>
     </div>
-</div>
-</body>
-</html>
+</x-layout.app>
